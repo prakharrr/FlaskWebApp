c568b8611288218f814aadb0532462e709237bf2
prakharrr <pxr7185@g.rit.edu>
2018-03-25 01:23:43 -0400

    added openweatherAPI for 14623

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ import json
 import urllib
 import urllib.request
 
-RSS_FEEDS = {'Best of abc.net.au': 'http://abc.net.au/bestof/bestofabc.xml', 'Radio Australia: Asia Pacific': 'http://abc.net.au/ra/rss/asiapacific.rss',
-             'ABC iView Programs': 'http://tvmp.abc.net.au/iview/rss/category/abc1.xml', 'ABC Hobart': 'http://www.abc.net.au/local/rss/hobart/news.xml',
+RSS_FEEDS = {'Best of abc.net.au': 'http://abc.net.au/bestof/bestofabc.xml',
+             'Radio Australia: Asia Pacific': 'http://abc.net.au/ra/rss/asiapacific.rss',
+             'ABC iView Programs': 'http://tvmp.abc.net.au/iview/rss/category/abc1.xml',
+             'ABC Hobart': 'http://www.abc.net.au/local/rss/hobart/news.xml',
              'First Dog on the Moon': 'http://www.abc.net.au/radionational/feed/5086350/rss.xml'}
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
 
 @app.route('/weather')
 def get_weather(query):
@@ -28,12 +30,17 @@ def get_weather(query):
     parsed = json.loads(data.decode("utf-8"))
     weather = None
     if parsed.get("weather"):
-        weather = {"description":
-                       parsed["weather"][0]["description"],
+        weather = {"description": parsed["weather"][0]["description"],
                    "temperature": parsed["main"]["temp"],
                    "city": parsed["name"]
                    }
     return render_template('weather.html')
+
+
+@app.route('/filter')
+def test():
+    return render_template('filter.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
